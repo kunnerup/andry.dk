d997d1d3bf1c977602ba7632ce261f3fef384d29
Anders Kunnerup <kunnerup16@hotmail.com>
2019-12-10 10:37:40 +0100

    Prices

--- js/js.js
+++ js/js.js
@@ -1,6 +1,17 @@
 //JAVASCRIPT
+
+//Burgermenuen. Tester om siden er klar og starter funktionen: ved klik
+//på burgermenu skal mobilemenu toggle frem som slid.. Langsomt
+//rammes med # eller . efter om det er class eller ID
 $(document).ready(function(){
 $("#burgermenu").click(function(){
 $(".mobileMenu").slideToggle("slow");
 });
+});
+
+//Samme som ovenstående - en slider der åbner prislisten.
+$(document).ready(function(){
+  $("#skubud").click(function(){
+    $("#priser").slideToggle("slow");
+  });
 });
